11407f1235b64c46105c8ba4a888a18a5ea14111
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-20 23:35:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def summon_captain_planet(array)# code an argument here
 end
 def long_planeteer_calls(array)# code an argument here
   i = 0  # Your code here
-  if array.any? { |i| i.length > 3 }
+  if array.any? { |i| i.length > 4 }
     return true
   else
     return false
