e871613e27ea9f692d5efdd106ac4869aa268cb3
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-20 23:34:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,13 @@ def summon_captain_planet(array)# code an argument here
   end
   produce
 end
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  i = 0  # Your code here
+  if array.any? { |i| i.length > 3 }
+    return true
+  else
+    return false
+  end
 end
 
 def find_the_cheese# code an argument here
